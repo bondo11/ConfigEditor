89d037823c9e16e7e4b36373c23479add476b56b
bondo11 <mbh@bysted.dk>
2014-11-24 08:29:16 +0100

    add missing files

---
+++ ConfigEditor/Folder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigEditor
+{
+    public class Folder
+    {
+        public string Path { get; set; }
+        public List<ruleset> rules = new List<ruleset>();
+
+        public Folder(string path)
+        {
+            Path = path;
+        }
+        public Folder(string path, List<ruleset> rulesets)
+        {
+            Path = path;
+            rules = rulesets;
+        }
+    }
+}
---
+++ ConfigEditor/UserControls/ActionUC/ucDeleteAction.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ConfigEditor
+{
+    /// <summary>
+    /// Interaction logic for ucDeleteAction.xaml
+    /// </summary>
+    public partial class ucDeleteAction : UserControl
+    {
+
+        public ucDeleteAction()
+        {
+            InitializeComponent();
+        }
+
+
+        public ucDeleteAction(Action act) : this()
+        {
+            // TODO: Complete member initialization
+            //tbPath.Text= moveAction.destination;
+        }
+    }
+}
