de4b63ac931c6f74db5f376983f06d299b8315d6
Ask Bisgaard <Elmiond@gmail.com>
2014-11-30 09:49:56 +0100

    slightly changed visuals, more to do

--- ConfigEditor/Windows/EditMatchSets.xaml.cs
+++ ConfigEditor/Windows/EditMatchSets.xaml.cs
@@ -37,36 +37,60 @@ namespace ConfigEditor
 
         private void listMatchsets_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            listMatch.Items.Clear();
             foreach (MatchSet ms in RulesCollection.matchSets)
             {
                 if (ms.Name.Equals(listMatchsets.SelectedValue))
-                {                   
+                {
                     switch (ms.match.kind)
                     {
                         case RulesCollection.MatchKinds.And:
-                            listMatch.Items.Add(new ucMatch((AndRule)ms.match));
+                            MatchContainer.Content = new ucMatch((AndRule)ms.match);
                             break;
                         case RulesCollection.MatchKinds.Or:
-                            listMatch.Items.Add(new ucMatch((OrRule)ms.match));
+                            MatchContainer.Content = new ucMatch((OrRule)ms.match);
                             break;
                         case RulesCollection.MatchKinds.extensionMatch:
-                            listMatch.Items.Add(new ucMatch((extensionMatch)ms.match));
+                            MatchContainer.Content = new ucMatch((extensionMatch)ms.match);
                             break;
                         case RulesCollection.MatchKinds.regexMatch:
-                            listMatch.Items.Add(new ucMatch((regexMatch)ms.match));
+                            MatchContainer.Content = new ucMatch((regexMatch)ms.match);
                             break;
                         default:
-                            listMatch.Items.Add(new ucMatch(ms.match));
+                            MatchContainer.Content = new ucMatch(ms.match);
                             break;
                     }
                 }
             }
+            //listMatch.Items.Clear();
+            //foreach (MatchSet ms in RulesCollection.matchSets)
+            //{
+            //    if (ms.Name.Equals(listMatchsets.SelectedValue))
+            //    {                   
+            //        switch (ms.match.kind)
+            //        {
+            //            case RulesCollection.MatchKinds.And:
+            //                listMatch.Items.Add(new ucMatch((AndRule)ms.match));
+            //                break;
+            //            case RulesCollection.MatchKinds.Or:
+            //                listMatch.Items.Add(new ucMatch((OrRule)ms.match));
+            //                break;
+            //            case RulesCollection.MatchKinds.extensionMatch:
+            //                listMatch.Items.Add(new ucMatch((extensionMatch)ms.match));
+            //                break;
+            //            case RulesCollection.MatchKinds.regexMatch:
+            //                listMatch.Items.Add(new ucMatch((regexMatch)ms.match));
+            //                break;
+            //            default:
+            //                listMatch.Items.Add(new ucMatch(ms.match));
+            //                break;
+            //        }
+            //    }
+            //}
         }
 
         private void btnAddMatch_Click(object sender, RoutedEventArgs e)
         {
-            listMatch.Items.Add(new ucMatch());
+            //listMatch.Items.Add(new ucMatch());
         }
 
         private void addbtnMatchsets_Click(object sender, RoutedEventArgs e)
@@ -96,7 +120,9 @@ namespace ConfigEditor
             String n = RulesCollection.matchSets[listMatchsets.SelectedIndex].Name;
             int i = (int)RulesCollection.matchSets[listMatchsets.SelectedIndex].match.kind;
 
-            RulesCollection.matchSets[listMatchsets.SelectedIndex].match = ((ucMatch)listMatch.Items[0]).GetMatch();
+
+            RulesCollection.matchSets[listMatchsets.SelectedIndex].match = ((ucMatch)MatchContainer.Content).GetMatch();
+            //RulesCollection.matchSets[listMatchsets.SelectedIndex].match = ((ucMatch)listMatch.Items[0]).GetMatch();
         }
 
     }
